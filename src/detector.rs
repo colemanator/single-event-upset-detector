39446522a01b1bd14a1043d9b0b784072c6acd5c
Colemanator <peter.coleman@rexsoftware.com.au>
2021-10-29 15:37:57 +1000

    Added Tests

--- src/detector.rs
+++ src/detector.rs
@@ -14,7 +14,11 @@ impl SingleEventUpset {
         // get the value of each bit
         let mut bits = [0; 64];
         for (index, bit) in (0..63).map(|n| num & (1 << n)).enumerate() {
-            bits[index] = bit as u8;
+            if bit > 0 {
+                bits[index] = 1;
+            } else {
+                bits[index] = 0;
+            }
         }
 
         SingleEventUpset {time: SystemTime::now(), bits, address: format!("{:p}", num), value: *num}
@@ -76,4 +80,60 @@ impl Detector {
     }
 
     pub fn get_num_bytes(&self) -> usize { self.simd_vecs.len() * 8 * 8 }
+}
+
+#[cfg(test)]
+mod test {
+    use core_simd::u64x8;
+    use crate::detector::{Detector, SingleEventUpset};
+
+    #[test]
+    fn can_get_upsets() {
+        let mut simd_vecs = vec![u64x8::splat(0); 5];
+        simd_vecs.push(u64x8::from_array([0, 0, 0, 0, 0, 0, 0, 1]));
+
+        let detector = Detector::from_vec(simd_vecs);
+
+        assert!(!detector.get_upsets().is_empty());
+    }
+
+    #[test]
+    fn can_get_no_upsets() {
+        let simd_vecs = vec![u64x8::splat(0); 5];
+        let detector = Detector::from_vec(simd_vecs);
+
+        assert!(detector.get_upsets().is_empty());
+    }
+
+    #[test]
+    fn can_reset() {
+        let simd_vecs = vec![u64x8::splat(1); 5];
+        let mut detector = Detector::from_vec(simd_vecs);
+
+        detector.reset();
+
+        assert!(detector.get_upsets().is_empty());
+    }
+
+    #[test]
+    fn can_get_num_bytes() {
+        let simd_vecs = vec![u64x8::splat(0); 5];
+        let detector = Detector::from_vec(simd_vecs);
+
+        assert_eq!(320, detector.get_num_bytes());
+    }
+
+    #[test]
+    fn can_create_single_event_upset() {
+        let num = 42;
+        let upset = SingleEventUpset::new(&num);
+
+        let bits: [u8; 64] = [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        assert_eq!(42, upset.value);
+        assert_eq!(bits, upset.bits);
+        assert_eq!(format!("{:p}", &num), upset.address);
+    }
 }
